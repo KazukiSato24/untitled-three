a16803acaf52bcb9dc6d52187c955e6ecc6fceef
KazukiSato <kazukisato970817@gmail.com>
2022-10-10 18:26:16 +0900

    add: camera

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 import * as THREE  from "./build/three.module.js";
 
-let scene;
+let scene, camera;
 
 function init() {
   scene = new THREE.Scene();
+
+  camera = new THREE.PerspectiveCamera( 
+    50,
+    innerWidth / innerHeight,
+    0.1,
+    3000
+  );
 }
