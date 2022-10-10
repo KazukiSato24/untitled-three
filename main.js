da665430150648e5204cbae7f2de41bcc8de4fcf
KazukiSato <kazukisato970817@gmail.com>
2022-10-10 18:52:52 +0900

    add: material

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import * as THREE  from "./build/three.module.js";
 
 let scene, camera, renderer;
 
+window.addEventListener("load", init);
+
 function init() {
   scene = new THREE.Scene();
 
@@ -14,7 +16,9 @@ function init() {
 
   renderer = new THREE.WebGLRenderer();
   document.body.appendChild(renderer.domElement);
-  renderer.setSize(Window.innerWidth, window.innerHeight);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
 
   renderer.render(scene, camera);
 };
