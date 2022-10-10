ba5f61e3bc4993d00ef4758bb30144417088f57d
KazukiSato <kazukisato970817@gmail.com>
2022-10-10 21:11:30 +0900

    fix: material

--- main.js
+++ main.js
@@ -17,9 +17,15 @@ function init() {
   camera.position.set(0, 5, 30);
   renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true});
   document.body.appendChild(renderer.domElement);
+  renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
 
-  const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+  const material = new THREE.MeshPhysicalMaterial({
+    color: 808080,
+    roughness: 0,
+    clearcoat: 1,
+    metalness: 0.5
+  });
 
   const rectangleBox = new THREE.Mesh(new THREE.BoxGeometry (15, 0.5, 15), material);
   scene.add(rectangleBox);
@@ -27,6 +33,6 @@ function init() {
   const sphere = new THREE.Mesh (new THREE.SphereGeometry(2, 36, 30), material);
   sphere.position.set(0, 2, 0);
   scene.add(sphere);
-
+  
   renderer.render(scene, camera);
 };
